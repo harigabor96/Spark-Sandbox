9e3aba5c34ce7a6fab2b4516bad6c765d7cd1204
Hári Gábor <gabor.hari@ecobit.hu>
2022-11-15 12:58:28 +0100

    router better formatting

--- Spark-Sandbox/src/main/scala/org/module/init/Router.scala
+++ Spark-Sandbox/src/main/scala/org/module/init/Router.scala
@@ -7,9 +7,11 @@ object Router {
 
   def executePipeline(spark: SparkSession, conf: Conf): Unit = conf.pipeline() match {
     case "sandbox-pipeline" =>
-      bronzesilvergold.tables.sandbox_table.Pipeline(spark, conf.rawZonePath(), conf.curatedZonePath()).execute()
+      bronzesilvergold.tables.sandbox_table
+        .Pipeline(spark, conf.rawZonePath(), conf.curatedZonePath()).execute()
     case "sandbox-query" =>
-      platinum.queries.sandbox_query.Query(spark, conf.curatedZonePath()).execute().show(false)
+      platinum.queries.sandbox_query
+        .Query(spark, conf.curatedZonePath()).execute().show(false)
     case _ =>
       throw new Exception("Pipeline is not registered in the router!")
   }
